1b39dfce3016fd8cf73ad44ca0d621d70b632895
Beth <bethm.meeker@gmail.com>
2021-07-05 16:52:42 -0600

    Pass props to Cohort, pass props from Cohort to Person

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,6 +2,22 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = ({staff}) => {
+  const peopleCards = staff.map(person => {
+    return (
+      <div>
+        <h1>Staff</h1>
+        <Person 
+          photo={person.photo}
+          name={person.name}
+          quote={person.quote}
+          superlative={person.superlative}
+          id={person.id}
+        />
+      </div>
+    );
+  }); 
+
+}
 
 export default Cohort;
