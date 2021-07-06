a041b69ec1efe68fba14d164e01cdc551e204f35
Beth <bethm.meeker@gmail.com>
2021-07-05 18:01:02 -0600

    Add inputs for name, quote, superlative and add button for submit

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,12 +13,16 @@ class Form extends Component {
     return (
       <div className='form-area'>
         <h2>Add A Student</h2>
-        <label>
-          <input type='text' />
+        <label for='name'>
+          <input type='text' placeholder='Name...' id='name' />
         </label>
-        <label>
-          <input type='text' />
+        <label for='quote'>
+          <input type='text' placeholder='Quote...' id='quote' />
         </label>
+        <label for='superlative'>
+          <input type='text' placeholder='Superlative...' id='superlative' />
+        </label>
+        <button className='submit-btn'>Submit</button>
       </div>
     );
   }
